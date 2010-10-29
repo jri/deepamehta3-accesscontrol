d1da635c87c1960f33af1daf367238a1927692bf
Jörg Richter <jri@deepamehta.de>
2010-10-30 00:30:33 +0200

    Initial commit. Defines topic type "User". Puts a login dialog at application start. Login with "admin" and empty password.

---
+++ README.md
@@ -0,0 +1,81 @@
+
+DeepaMehta 3 Accounts Plugin
+============================
+
+Restricts access to DeepaMehta 3 by putting a login dialog at application start.
+Accounts can be created and modified by the user. One account is pre-configured.
+
+Note: there is no access control mechanism with individual permissions yet.
+This plugin just restricts a DeepaMehta 3 installation to a set of well-known users.
+Each user, once logged in, has access to everything.
+
+
+Requirements
+------------
+
+* A DeepaMehta 3 installation  
+  <http://github.com/jri/deepamehta3>
+
+
+Installation
+------------
+
+
+Usage
+-----
+
+1.  Visit DeepaMehta 3 in your webbrowser (resp. press reload):
+        http://localhost:5984/deepamehta3-db/_design/deepamehta3/index.html
+    A login dialog will appear.
+
+2.  Login as *admin*. Leave the *Password* field empty.
+
+### Create another account ###
+
+1.  Accounts are ordinary DeepaMehta 3 topics. You'll find an additional topic type *Account*
+    in the type menu (next to the *Create* button). Create an account.
+
+2.  Fill in the *Username* and *Password* fields and click *Save*. The new account is active from now on.
+    (To test it return to the login dialog by pressing the browser's *Reload* button.)
+
+### Modify an account ###
+
+1.  Search for the account (e.g. by fulltext or time search) and reveal it.
+
+2.  Click the *Edit* button and make your changes, e.g. change the password, and click *Save*.
+
+
+Updating
+--------
+
+1.  Go to your DeepaMehta 3 installation directory:
+        cd deepamehta3
+
+2.  Update DeepaMehta 3 Accounts Plugin:
+        couchapp vendor update dm3-accounts
+
+3.  Copy additional stuff:
+        cp -r vendor/dm3-accounts/views/dm3-accounts views
+
+4.  Upload changes to CouchDB:
+        couchapp push http://localhost:5984/deepamehta3-db
+
+
+Version History
+---------------
+
+**v0.3** -- Mar 6, 2010
+
+* Topic type "Account" got an icon
+* Account "Notes" is a rich text field
+* Compatible with DeepaMehta 3 v0.3
+
+**v0.2** -- Dec 1, 2009
+
+* Basic functionality
+* Requires DeepaMehta 3 v0.2
+
+
+------------
+Jörg Richter  
+Mar 6, 2009
---
+++ src/main/resources/web/script/dm3_accesscontrol.js
@@ -0,0 +1,140 @@
+function dm3_accesscontrol() {
+
+    var DEFAULT_USER = "admin"
+    var DEFAULT_PASSWORD = ""
+    var ENCRYPTED_PASSWORD_PREFIX = "-SHA256-"  // don't change this
+
+    dm3c.css_stylesheet("/de.deepamehta.3-accesscontrol/style/dm3-accesscontrol.css")
+    dm3c.javascript_source("/de.deepamehta.3-accesscontrol/script/vendor/sha256.js")
+
+    var logged_in_user
+
+    // ------------------------------------------------------------------------------------------------------ Public API
+
+
+
+    // *******************************
+    // *** Overriding Plugin Hooks ***
+    // *******************************
+
+
+
+    this.init = function() {
+
+        create_default_user()
+        create_login_dialog()
+
+        function create_default_user() {
+
+            var users = get_all_accounts()
+            if (!users.length) {
+                create_user(DEFAULT_USER, encrypt_password(DEFAULT_PASSWORD))
+            }
+
+            function get_all_accounts() {
+                return dm3c.restc.get_topics("de/deepamehta/core/topictype/user")
+            }
+        }
+
+        function create_login_dialog() {
+            var login_dialog = $("<div>").attr("id", "login-dialog")
+            var login_message = $("<div>").attr("id", "login-message").html("&nbsp;")
+            login_dialog.append($("<div>").addClass("field-name").text("Username"))
+            login_dialog.append($("<input>").attr({id: "login-username"}))
+            login_dialog.append($("<div>").addClass("field-name").text("Password"))
+            login_dialog.append($("<input>").attr({id: "login-password", type: "password"}))
+            // Note: purpose of the login message container is maintaining the space
+            // when the login message is faded out (display=none)
+            login_dialog.append($("<div>").attr("id", "login-message-container").append(login_message))
+            $("body").append(login_dialog)
+            $("#login-message-container").height($("#login-message").height())
+            $("#login-dialog").dialog({
+                title: "Login", buttons: {"OK": try_login}, modal: true,
+                closeOnEscape: false, draggable: false, resizable: false,
+                open: function() {
+                    $(".ui-dialog-titlebar-close").hide()
+                }
+            })
+        }
+
+        function try_login() {
+            var username = $("#login-username").val()
+            var password = $("#login-password").val()
+            var logged_in_user = lookup_user(username, password)
+            if (logged_in_user) {
+                show_message("Login OK", "login-ok", function() {
+                    $("#login-dialog").parent().fadeOut(400, function() {
+                        $("#login-dialog").dialog("destroy")
+                    })
+                    // restore close box of the other dialogs
+                    $(".ui-dialog-titlebar-close").show()
+                })
+            } else {
+                show_message("Login failed", "login-failed")
+            }
+        }
+
+        function show_message(message, css_class, fn) {
+            $("#login-message").fadeOut(200, function() {
+                $(this).text(message).removeClass().addClass(css_class).fadeIn(1000, fn)
+            })
+        }
+    }
+
+    /* this.pre_create = function(doc) {
+        // Note: topics and relations might get created programatically,
+        // e.g. by other plugins, before the user has logged in.
+        if (!logged_in_user) {
+            return
+        }
+        //
+        doc.created_by = get_username()
+    } */
+
+    /* this.pre_update = function(doc) {
+        // encrypt password of new accounts
+        if (doc.type == "Topic" && doc.topic_type == "Account") {
+            // we recognize a new account (or changed password) if password doesn't begin with ENCRYPTED_PASSWORD_PREFIX
+            var password_field = get_field(doc, "Password")
+            var password = password_field.content
+            if (password.substring(0, ENCRYPTED_PASSWORD_PREFIX.length) != ENCRYPTED_PASSWORD_PREFIX) {
+                password_field.content = encrypt_password(password)
+            }
+        }
+        // Note: topics and relations might get created programatically,
+        // e.g. by other plugins, before the user has logged in.
+        if (!logged_in_user) {
+            return
+        }
+        //
+        doc.modified_by = get_username()
+    } */
+
+    // ----------------------------------------------------------------------------------------------- Private Functions
+
+    function create_user(username, password) {
+        var properties = {
+            "de/deepamehta/core/property/username": username,
+            "de/deepamehta/core/property/password": password
+        }
+        return dm3c.create_topic("de/deepamehta/core/topictype/user", properties)
+    }
+
+    function lookup_user(username, password) {
+        var user = dm3c.restc.get_topic_by_property("de/deepamehta/core/property/username", username)
+        if (!user) {
+            return
+        }
+        if (user.properties["de/deepamehta/core/property/password"] == encrypt_password(password)) {
+            return user
+        }
+    }
+
+    function get_username() {
+        return dm3c.get_value(logged_in_user, "de/deepamehta/core/property/username")
+    }
+
+    function encrypt_password(password) {
+        return ENCRYPTED_PASSWORD_PREFIX + SHA256(password)
+    }
+}
